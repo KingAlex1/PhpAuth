42604e56c351cbfc2cbf2f398a1f3eb69e0f33c2
Alex B <alex-sert2015@mail.ru>
2018-03-30 20:30:58 +0300

    Added migrations

---
+++ migration/migrationOrders.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+require_once "../App/models/Order.php";
+
+Capsule::schema()->dropIfExists('orders');
+
+Capsule::schema()->create('orders', function (Blueprint $table) {
+    $table->increments('id');
+    $table->integer('user_id')->nullable();
+    $table->string('adress'); //varchar 255
+    $table->string('phone'); //varchar 255
+    $table->string('mail'); //varchar 255
+    $table->string('good'); //varchar 255
+    $table->string('photo'); //varchar 255
+    $table->string('desc'); //varchar 255
+
+});
---
+++ migration/migrationUsers.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+require_once "../App/models/User.php";
+
+Capsule::schema()->dropIfExists('users');
+
+Capsule::schema()->create('users', function (Blueprint $table) {
+    $table->increments('id');
+    $table->string('login'); //varchar 255
+    $table->string('password'); //varchar 255
+    $table->string('name'); //varchar 255
+    $table->integer('age');
+    $table->string('description'); //varchar 255
+    $table->string('photo'); //varchar 255
+});
+
+
